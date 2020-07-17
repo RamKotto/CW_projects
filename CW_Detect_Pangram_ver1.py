6ba334c193789b26fa7d03df70a4a94a680fbc34
RamKotto <68374471+RamKotto@users.noreply.github.com>
2020-07-17 23:11:42 +0300

    Update CW_Detect_Pangram_ver1.py

--- CW_Detect_Pangram_ver1.py
+++ CW_Detect_Pangram_ver1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 s = 'Pack my box with five dozen liquor jugs'
 
 def is_pangram(s):
@@ -15,21 +14,3 @@ def is_pangram(s):
 
 
 print(is_pangram(s))
-=======
-s = 'Pack my box with five dozen liquor jugs'
-
-def is_pangram(s):
-    s = s.lower()
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    count = 0
-    for i in alphabet:
-        if i in s:
-            count += 1
-    if count == 26:
-        return True
-    else:
-        return False
-
-
-print(is_pangram(s))
->>>>>>> c3f95e5cd5a45918ce2152d4c42e23f8194e6df1
