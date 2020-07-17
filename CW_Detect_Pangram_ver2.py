dea806aa22036fd83310cfc5345e882300d7d463
RamKotto <68374471+RamKotto@users.noreply.github.com>
2020-07-17 23:10:51 +0300

    Update CW_Detect_Pangram_ver2.py

--- CW_Detect_Pangram_ver2.py
+++ CW_Detect_Pangram_ver2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 s = 'Pack my box with five dozen liquor jugs'
 
 import string
@@ -7,13 +6,3 @@ def is_pangram(s):
     return set(string.ascii_lowercase) <= set(s.lower())
 
 print(is_pangram(s))
-=======
-s = 'Pack my box with five dozen liquor jugs'
-
-import string
-
-def is_pangram(s):
-    return set(string.ascii_lowercase) <= set(s.lower())
-
-print(is_pangram(s))
->>>>>>> c3f95e5cd5a45918ce2152d4c42e23f8194e6df1
