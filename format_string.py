c85fd712a4f2d384ef7a75aa819a3120cff668e8
RamKotto <68374471+RamKotto@users.noreply.github.com>
2020-08-31 23:06:51 +0300

    Update format_string.py

--- format_string.py
+++ format_string.py
@@ -6,7 +6,7 @@ print(secret_password)
 # обрамляет слово символам
 word = input('Какое слово будем заключать в символы: ')
 how_many = int(input('Сколько символов нужно по краям: '))
-symbol = input('В какие символыб бро: ')
+symbol = input('В какие символы: ')
 
 def word_in_symbols(word, how_many, symbol):
     qty = len(word) + (how_many * 2)
